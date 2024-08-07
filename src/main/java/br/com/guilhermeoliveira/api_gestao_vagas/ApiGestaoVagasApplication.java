a7a4aafbe931e6be45bca2b31363d04b7df209a5
gholiveira29 <g.h.p.o3615@gmail.com>
2024-08-07 18:20:01 -0300

    estrutura do projeto

---
+++ src/main/java/br/com/guilhermeoliveira/api_gestao_vagas/ApiGestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermeoliveira.api_gestao_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestaoVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guilhermeoliveira/api_gestao_vagas/ApiGestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermeoliveira.api_gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
