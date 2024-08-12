344751fa411cdd957464a43044e41b94a902616d
gholiveira29 <g.h.p.o3615@gmail.com>
2024-08-12 19:05:50 -0300

    criando banco de dados

---
+++ src/main/java/br/com/guilhermeoliveira/api_gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.guilhermeoliveira.api_gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
--- src/main/java/br/com/guilhermeoliveira/api_gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/guilhermeoliveira/api_gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,42 @@
 package br.com.guilhermeoliveira.api_gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "the field usarname must not contain spaces")
     private String username;
+
+    @Email(message = "The field must contain a valid email adress")
     private String email;
+
+    @Length(min = 10, max = 20, message = "Password must contain between 10 end 20 characters")
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
 }
